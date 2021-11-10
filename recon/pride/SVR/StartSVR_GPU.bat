75465e93fd7872d5bc6e27e07c31c92e720fff7a
tomaroberts <t.roberts@kcl.ac.uk>
2021-11-10 19:08:44 +0000

    docker --rm containers after they have run.

--- recon/pride/SVR/StartSVR_GPU.bat
+++ recon/pride/SVR/StartSVR_GPU.bat
@@ -4,7 +4,7 @@
 
 :: Docker image name (default: svrtk-docker-gpu)
 set hostName=%COMPUTERNAME%
-set "dockerImageName=fetalsvrtk/svrtk:svrtk-docker-gpu"
+set "dockerImageName=fetalsvrtk/svrtk:pride-svr-docker-0.10"
 ECHO Using Docker Image: %dockerImageName%
 
 :: Clear TempOutputSeries
@@ -15,20 +15,20 @@ ECHO DONE
 
 :: Convert DICOMs from PRIDE to nifti, and, copy to recon folder
 ECHO Converting DICOMs to nifti ...
-wsl.exe docker run -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/svr_dcm2nii.bash > C:\svrtk-docker-gpu\recon\pride\logs\log_svr_dcm2nii.txt
+wsl.exe docker run --rm -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/svr_dcm2nii.bash > C:\svrtk-docker-gpu\recon\pride\logs\log_svr_dcm2nii.txt
 ECHO DONE
 
 :: Run SVRTK GPU Docker reconstruction
 ECHO Running SVR reconstruction ...
-wsl.exe docker run -it --gpus all -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/docker-recon-brain-auto.bash /home/recon/ 1 -1 > C:\svrtk-docker-gpu\recon\pride\logs\log_svrtk_docker_gpu.txt
+wsl.exe docker run --rm --gpus all -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/docker-recon-brain-auto.bash /home/recon/ 1 -1 > C:\svrtk-docker-gpu\recon\pride\logs\log_svrtk_docker_gpu.txt
 ECHO DONE
 
 :: Convert SVR nifti to DICOM, and, copy to TempOutputSeries
 ECHO Converting SVR nifti to DICOM ...
-wsl.exe docker run -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% python /home/scripts/svr_nii2pridedcm.py > C:\svrtk-docker-gpu\recon\pride\logs\log_svr_nii2pridedcm.txt
+wsl.exe docker run --rm -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% python /home/scripts/svr_nii2pridedcm.py > C:\svrtk-docker-gpu\recon\pride\logs\log_svr_nii2pridedcm.txt
 
 :: Create DICOMDIR file
-wsl.exe docker run -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/svr_make_dicomdir.bash >> C:\svrtk-docker-gpu\recon\pride\logs\log_svr_nii2pridedcm.txt
+wsl.exe docker run --rm -v "/mnt/c/svrtk-docker-gpu/recon":/home/recon %dockerImageName% /home/scripts/svr_make_dicomdir.bash >> C:\svrtk-docker-gpu\recon\pride\logs\log_svr_nii2pridedcm.txt
 ECHO DONE
 
 :: Save Recon Directory
